baa453d4d81aa3dd6967f00eb7a2e00cdc29a067
苏震 <suzhen@viewhigh.com>
2019-09-16 09:22:17 +0800

    课程表更新
    课程表更新

--- pages/fee/index.js
+++ pages/fee/index.js
@@ -4,11 +4,10 @@ const app = getApp()
 
 Page({
   data: {
+    feeList: [
+      { date: '2019-09-11', project: '足球网兜', amount: 331.8, report: '梁若歆妈妈', remain: 3960.4 },
+      { date: '2019-09-09', project: '教师节礼物', amount: 358.0, report: '张雅林妈妈', remain: 4292.2 },
+      { date: '2019-09-08', project: '班主任班服', amount: 149.8, report: '刘润博妈妈', remain: 4650.2 }
+    ]
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  }
 })
